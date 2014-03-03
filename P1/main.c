79a40b781f8473c450b81cc21de520b64f77adbe
Fabian92mx <fabian92_mex@hotmail.com>
2014-03-02 22:42:33 -0600

    agregado doxyfile

--- P1/main.c
+++ P1/main.c
@@ -21,58 +21,63 @@ int debugLevel = 0;
 
 int main(int argc, char *argv[]) {
 
-printf(".:: Small Hard Coded Web Server ::.\n");
-if(!processArgs(argc, argv)) {
-return 1;
-}
+	printf(".:: Small Hard Coded Web Server ::.\n");
+	if(!processArgs(argc, argv)) {
+	return 1;
+	}
 
-debug(2,"Puerto: %u",port);
-startServer(port);
+	debug(2,"Puerto: %u",port);
+	startServer(port);
 
-return 0;
+	return 0;
 }
 
 int processArgs(int argc, char *argv[]) {
 
-int i;
-unsigned int temp;
+	int i;
+	unsigned int temp;
 
-for(i=1; i < argc; i++) {
+	for(i=1; i < argc; i++) {
 
-if(strcmp(argv[i],"-p")==0) {
-temp = strtoimax(argv[++i], NULL, 10);
-if(temp == 0 || temp < MINPORT || temp > MAXPORT) {
-printHelp(TRUE,argv[0],"Puerto fuera de rango\n");
-return FALSE;
-}
-port = temp;
-}
+		if(strcmp(argv[i],"-p")==0) {
+			temp = strtoimax(argv[++i], NULL, 10);
+			if(temp == 0 || temp < MINPORT || temp > MAXPORT) {
+				printHelp(TRUE,argv[0],"Puerto fuera de rango\n");
+				return FALSE;
+				}
+			port = temp;
+			}
 
-else if(strstr(argv[i],"-v")!=NULL) {
-debugLevel = strlen(argv[i])-1;
-}
+			else if(strstr(argv[i],"-v")!=NULL) {
+				debugLevel = strlen(argv[i])-1;
+			}
 
-else {
-printHelp(TRUE ,argv[0],"Opción no valida\n");
-return FALSE;
-}
+		else {
+			printHelp(TRUE ,argv[0],"Opción no valida\n");
+			return FALSE;
+			}
 
-}
+		}
 
-return TRUE;
+	return TRUE;
 }
 
 
 void printHelp(int is_error, char *cmd, char *error) {
 
-if(is_error) {
-printf("Error: %s\n",error);
-}
+	if(is_error) {
+		printf("Error: %s\n",error);
+		}
 
 printf("USE:\t%s [OPTIONS]\n",cmd);
+
 printf("\nOPCIONES:\n");
+
 printf("\t-p\t Numero de Puerto (Default %u)\n",CONFIG_DEFAULT_PORT);
+
 printf("\t-v[vvvv]\t Nivel de Verbosity\n");
+
 printf("\n");
+
 return;
 }
--- P1/server.c
+++ P1/server.c
@@ -1,7 +1,11 @@
 /**
-*
-**/
+@file server.c
+@brief Funciones generales para trabajar con un Socket de tipo TCP
 
+@authors Javier de la Mora y Fabián Escobar
+@date Mar/2014
+
+*/
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
@@ -17,102 +21,102 @@ void clientProccess(const int clientSocket);
 
 int startServer(const unsigned int port) {
 
-int serverSocket;
-int clientSocket;
-char clientIP[16];
-unsigned int clientPort;
+	int serverSocket;
+	int clientSocket;
+	char clientIP[16];
+	unsigned int clientPort;
 
-int pid;
+	int pid;
 
-serverSocket = newTCPServerSocket4("0.0.0.0",port,5);
-if(serverSocket == -1) {
-error(errno,"Can't create Socket");
-return FALSE;
-}
-debug(4,"Server Socket Created");
+	serverSocket = newTCPServerSocket4("0.0.0.0",port,5);
+	if(serverSocket == -1) {
+		error(errno,"Can't create Socket");
+		return FALSE;
+	}
+	debug(4,"Server Socket Created");
 
-while(TRUE) {
-clientSocket = waitConnection4(serverSocket,clientIP,&clientPort);
-debug(2,"Connected Client %s:%u",clientIP,clientPort);
+	while(TRUE) {
+		clientSocket = waitConnection4(serverSocket,clientIP,&clientPort);
+	debug(2,"Connected Client %s:%u",clientIP,clientPort);
 
-pid = fork();
-if(pid == -1) {
-debug(0,"Can't Fork!");
-close(clientSocket);
-continue;
-} else if(pid == 0 ) {
-// Soy el Hijo.
-clientProccess(clientSocket);
-} else if (pid > 0 ) {
-// Soy el Padre
-close(clientSocket);
-}
-    }
+	pid = fork();
+	if(pid == -1) {
+		debug(0,"Can't Fork!");
+		close(clientSocket);
+		continue;
+	} else if(pid == 0 ) {
+		// Soy el Hijo.
+		clientProccess(clientSocket);
+	} else if (pid > 0 ) {
+		// Soy el Padre
+		close(clientSocket);
+		}
+	}
 
-return TRUE;
+	return TRUE;
 
 }
 
 
 void clientProccess(const int clientSocket) {
 
-char *buffer;
-char *firstLine;
-char html[250];
-int file;
-int readBytes;
-int firstFlag;
+	char *buffer;
+	char *firstLine;
+	char html[250];
+	int file;
+	int readBytes;
+	int firstFlag;
 
-debug(2,"Inicio del proceso del Cliente\n");
+	debug(2,"Inicio del proceso del Cliente\n");
 
 
-buffer = calloc(255,1);
-firstLine = calloc(255,1);
-firstFlag = TRUE;
-while(readTCPLine4(clientSocket,buffer,254)>0) {
-debug(4,"%s",buffer);
-if(strcmp(buffer,"\r\n")==0) {
-break;
-}
-if(firstFlag) {
-strcpy(firstLine,buffer);
-firstFlag = FALSE;
-}
-bzero(buffer,255);
-}
+	buffer = calloc(255,1);
+	firstLine = calloc(255,1);
+	firstFlag = TRUE;
+	while(readTCPLine4(clientSocket,buffer,254)>0) {
+		debug(4,"%s",buffer);
+		if(strcmp(buffer,"\r\n")==0) {
+			break;
+		}
+		if(firstFlag) {
+			strcpy(firstLine,buffer);
+			firstFlag = FALSE;
+		}
+		bzero(buffer,255);
+	}
 
-// PROCESAR EL GET
-// TIP para esto: strtok
+	// PROCESAR EL GET
+	// TIP para esto: strtok
 
-//RESPONDER CON 200 OK SI EXISTE
-strcpy(html, "HTTP/1.1 200 OK\r\n");
-sendTCPLine4(clientSocket, html,strlen(html));
-//strcpy(html, "Content-Type: \r\n");
-//sendTCPLine4(clientSocket, html,strlen(html));
-//strcpy(html, "Content-Lenght: 457838592\r\n");
-//sendTCPLine4(clientSocket, html,strlen(html));
-strcpy(html, "\r\n");
-sendTCPLine4(clientSocket, html,strlen(html));
+	//RESPONDER CON 200 OK SI EXISTE
+	strcpy(html, "HTTP/1.1 200 OK\r\n");
+	sendTCPLine4(clientSocket, html,strlen(html));
+	//strcpy(html, "Content-Type: \r\n");
+	//sendTCPLine4(clientSocket, html,strlen(html));
+	//strcpy(html, "Content-Lenght: 457838592\r\n");
+	//sendTCPLine4(clientSocket, html,strlen(html));
+	strcpy(html, "\r\n");
+	sendTCPLine4(clientSocket, html,strlen(html));
 
-//ENVIAR EL ARCHIVO
-file = open("archivo.html",O_RDONLY);
-if(file == -1) {
-error(errno, "No se pudo abrir el archivo");
-}
+	//ENVIAR EL ARCHIVO
+	file = open("archivo.html",O_RDONLY);
+	if(file == -1) {
+		error(errno, "No se pudo abrir el archivo");
+	}
 
-while((readBytes = read(file,buffer,255))>0) {
-sendTCPLine4(clientSocket,buffer,readBytes);
-}
+	while((readBytes = read(file,buffer,255))>0) {
+		sendTCPLine4(clientSocket,buffer,readBytes);
+	}
 
-//RESPONDER CON 404 NOT FOUND SI NO EXISTE
+	//RESPONDER CON 404 NOT FOUND SI NO EXISTE
 
-//strcpy(html, "<html><head><title>404 NOT FOUND</title></head><body>NO EXISTE!!!</body></html>");
-//sendTCPLine4(clientSocket, html,strlen(html));
+	//strcpy(html, "<html><head><title>404 NOT FOUND</title></head><body>NO EXISTE!!!</body></html>");
+	//sendTCPLine4(clientSocket, html,strlen(html));
 
-//CERRAMOS LA COMUNICACIÓN
-close(clientSocket);
-free(buffer);
-free(firstLine);
+	//CERRAMOS LA COMUNICACIÓN
+	close(clientSocket);
+	free(buffer);
+	free(firstLine);
 
-return;
+	return;
 }
